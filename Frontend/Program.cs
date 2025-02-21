a8d374822058a06ca091fed80288f86f26dddc79
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2025-02-21 12:31:42 +0100

    Set up path.

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -8,8 +8,8 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddLogging();
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://api:8080/") });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8000/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://api:8080/") });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8000/") });
 builder.Services.AddScoped<TestService>();
 
 var app = builder.Build();
