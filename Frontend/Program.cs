6c1ee1c7e89ccc05fd5cbad3c2acf2a2de9cd6a8
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2025-02-21 18:21:37 +0100

    Small changes.

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -3,7 +3,6 @@ using Frontend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddLogging();
@@ -16,9 +15,13 @@ builder.Services.AddScoped<TestService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
